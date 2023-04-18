3d812d523731451214b25d048449f823012e4e96
ahmadkismael <ahmad.abualkhair@gmail.com>
2023-04-18 15:26:05 -0600

    fixed navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({ username }) => {
   return (
     <nav>
-      <Link to="/movies">Movies</Link> &nbsp; | &nbsp;{" "}
-      <Link to="/actors">Actors</Link>
+      <ul>
+        <li>
+          <Link to="/movies">Movies</Link>{" "}
+        </li>
+        &nbsp; | &nbsp;{" "}
+        <li>
+          <Link to="/actors">Actors</Link>
+        </li>
+        <li>{username}</li>
+      </ul>
     </nav>
   );
 };
